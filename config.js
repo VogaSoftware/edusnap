6f453afac5932bcbcb09323141d9827fbc414db6
Graham <gmdianaty@gmail.com>
2016-10-20 01:07:52 +0000

    We goin' back son

--- config.js
+++ config.js
@@ -8,8 +8,8 @@
 module.exports = {
     mongoURL : 'mongodb://ds041871.mlab.com:41871/heroku_q0t9ns39',
     mongoOptions : {
-        user : 'api',
-        pass : 'api'
+        user : 'heroku_q0t9ns39',
+        pass : 'heroku_q0t9ns39'
     },
     redisURL: 'redis://redistogo:0a1863e94daaac6aac68b369053d1eb1@sculpin.redistogo.com:10676/',
     deleteSnaps: true
